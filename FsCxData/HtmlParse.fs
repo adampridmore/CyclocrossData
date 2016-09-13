a84e097bb7030a5c995291674321f56f9e37bbb7
adam.pridmore <adam_pridmore@hotmail.com>
2016-09-13 10:15:28 +0100

    - Created a Laps type with lap time and cumulative lap time.

--- FsCxData/HtmlParse.fs
+++ FsCxData/HtmlParse.fs
@@ -1,6 +1,7 @@
 ﻿module HtmlParse
 
 open FSharp.Data
+open System
 
 [<Literal>] 
 let Url = @"http://results.smartiming.co.uk/view-race/ycca1617winterround2senior"
@@ -13,6 +14,18 @@ type LapColumn = {
     index: int; 
     lapNumber: int
 }  
+
+type Lap = {
+    lapTime : TimeSpan
+    cumulativeLapTime : TimeSpan
+}
+
+type Rider = {
+    name: string
+    laps : Lap list
+    lapCount : int
+}
+
 
 let toLapColumn index lapNumber = { index = index; lapNumber = lapNumber }
 
@@ -31,3 +44,46 @@ let lapColumns =
     |> Seq.filter(fun (_ , header) -> header.IsSome)
     |> Seq.map (fun (i, header) -> toLapColumn i header.Value)
 
+let parseTimeSpan (text:string) =
+    try
+        ("00:" + text) |> TimeSpan.Parse
+    with | _ -> failwithf "Error parsing %s" text
+
+let lapsToCumulative (laps: TimeSpan seq) =
+    let mapping (state:TimeSpan) (lap:TimeSpan) =
+        let total = lap + state
+        (total, total)
+    laps 
+    |> Seq.mapFold mapping TimeSpan.Zero
+    |> fst
+
+let parseRow (row:resultsProvider.Smartiming.Row) =
+    let name = sprintf "%s %s" row.``First Name`` row.Surname
+    
+    let lapTimes = 
+        [
+            row.OutLap;
+            row.Lap1;
+            row.Lap2;
+            row.Lap3;
+            row.Lap4;
+            row.Lap5;
+            row.Lap6;
+            row.Lap7;
+        ] 
+        |> Seq.filter (String.IsNullOrWhiteSpace >> not)
+        |> Seq.map parseTimeSpan
+        |> Seq.toList
+
+    let cumulativeLapTimes = lapTimes |> lapsToCumulative |> Seq.toList
+
+    let laps = 
+        Seq.zip lapTimes cumulativeLapTimes
+        |> Seq.map (fun (lapTime,cumulativeLapTime) -> { lapTime = lapTime; cumulativeLapTime = cumulativeLapTime})
+        |> Seq.toList
+
+    { 
+        name = name;
+        laps = laps;
+        lapCount = laps.Length;
+    }
--- FsCxData/Script_Html.fsx
+++ FsCxData/Script_Html.fsx
@@ -10,57 +10,10 @@ open FSharp.Charting
 open System
 open HtmlParse
 
-lapColumns
+let riders = table.Rows |> Seq.map parseRow
 
-
-type Rider = {
-    name: string
-    laps: TimeSpan list
-    cumulativeLapTime : TimeSpan list
-    lapCount : int
-}
-
-let parseTimeSpan (text:string) =
-    try
-        ("00:" + text) |> TimeSpan.Parse
-    with | _ -> failwithf "Error parsing %s" text
-
-let lapsToCumulative (laps: TimeSpan seq) =
-    let mapping (state:TimeSpan) (lap:TimeSpan) =
-        let total = lap + state
-        (total, total)
-    laps 
-    |> Seq.mapFold mapping TimeSpan.Zero
-    |> fst
-
-let parseRow (row:resultsProvider.Smartiming.Row) =
-    let name = sprintf "%s %s" row.``First Name`` row.Surname
-    
-    let laps = 
-        [
-            row.OutLap;
-            row.Lap1;
-            row.Lap2;
-            row.Lap3;
-            row.Lap4;
-            row.Lap5;
-            row.Lap6;
-            row.Lap7;
-        ] 
-        |> Seq.filter (String.IsNullOrWhiteSpace >> not)
-        |> Seq.map parseTimeSpan
-        |> Seq.toList
-
-    { 
-        name = name;
-        laps = laps;
-        lapCount = laps.Length;
-        cumulativeLapTime = laps |> lapsToCumulative |> Seq.toList
-    }
-
-table.Rows
-|> Seq.map parseRow
-|> Seq.map (fun (rider:Rider) -> rider.cumulativeLapTime |> Seq.map (fun ts -> ts.TotalSeconds))
+riders
+|> Seq.map (fun (rider:Rider) -> rider.laps |> Seq.map (fun lap -> lap.cumulativeLapTime.TotalSeconds))
 |> Seq.map FSharp.Charting.Chart.Line
 |> FSharp.Charting.Chart.Combine
 |> FSharp.Charting.Chart.Show

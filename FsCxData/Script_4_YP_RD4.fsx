1019d2add635b4afcfce22a264e5a54d1e2b01a9
adam.pridmore <adam_pridmore@hotmail.com>
2016-11-20 20:05:42 +0000

    - Added RD8 URL

--- FsCxData/Script_4_YP_RD4.fsx
+++ FsCxData/Script_4_YP_RD4.fsx
@@ -16,13 +16,16 @@ open Types
 open LapRender
 
 let urls = [
-    @"http://results.smartiming.co.uk/view-race/ycca1617winterround1senior"
-    @"http://results.smartiming.co.uk/view-race/ycca1617winterround2senior"
-    @"http://results.smartiming.co.uk/view-race/ycca1617winterround3senior/" // RD3
-    @"http://results.smartiming.co.uk/view-race/ycca1617winterround4senior/" // RD4
+//    @"http://results.smartiming.co.uk/view-race/ycca1617winterround1senior"
+//    @"http://results.smartiming.co.uk/view-race/ycca1617winterround2senior"
+//    @"http://results.smartiming.co.uk/view-race/ycca1617winterround3senior/" // RD3
+//    @"http://results.smartiming.co.uk/view-race/ycca1617winterround4senior/" // RD4
+//    @"http://results.smartiming.co.uk/view-race/ycca1617winterround5senior/" // RD5
+//    @"http://results.smartiming.co.uk/view-race/ycca1617winterround5youth/" // RD5 - Youth
+    @"http://results.smartiming.co.uk/view-race/ycca1617winterround8senior/"       // RD8
     ]
 
-urls.[3] |> Seq.singleton
+urls |> Seq.head |> Seq.singleton
 //urls
 |> Seq.map riderAndLapsFromHtml
 |> Seq.iter render

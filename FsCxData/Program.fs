f042e99803368c6550cc699e8d05528561c5e7f6
adam.pridmore <adam_pridmore@hotmail.com>
2021-10-03 20:05:52 +0100

    Added FsCharting.data + Charting (GTK)

---
+++ FsCxData/Program.fs
@@ -0,0 +1,17 @@
+// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+// Define a function to construct a message to print
+let from whom =
+    sprintf "from %s" whom
+
+[<EntryPoint>]
+let main argv =
+ // printfn "Hello World" 
+ // let riderAndLaps = riderAndLapsFromHtml()
+    // let riderAndLaps = riderAndLapsFromCsv()
+
+    // riderAndLaps |> render
+
+    0

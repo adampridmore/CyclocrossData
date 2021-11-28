4684929d8822f6c8019b83f6f056cb406b2beee4
Adam Pridmore <adam_pridomre@hotmmail.com>
2021-11-28 22:42:14 +0000

    Add Northern Area Championships MJ - MV40

--- FsCxData/Scripts/Script_10_YP_2021_RD3.fsx
+++ FsCxData/Scripts/Script_10_YP_2021_RD3.fsx
@@ -17,11 +17,14 @@ open LapRender
 
 // Winter 2021
 let urls = [
-    // @"http://results.smartiming.co.uk/view-race/yycard1mv40senior/";  // RD1 (Not on smart timings...)
-    // @"http://results.smartiming.co.uk/view-race/yycard2mv40senior/"; // RD2
-    @"http://results.smartiming.co.uk/view-race/yycard3mv40senior/"  // RD3
+    // @"http://results.smartiming.co.uk/view-race/yycard1mv40senior/";     // RD1 (Not on smart timings...)
+    // @"http://results.smartiming.co.uk/view-race/yycard2mv40senior/";     // RD2
+    // @"http://results.smartiming.co.uk/view-race/yycard3mv40senior/"      // RD3
+    "http://results.smartiming.co.uk/view-race/northernareachampsmjmv40/"   // Northern Area Championships MJ - MV40
 ]
 
 urls //|> Seq.head |> Seq.singleton
 |> Seq.map riderAndLapsFromHtml
 |> Seq.iter render
+
+

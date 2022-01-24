97da5ee8e256a755e20cfd7457d671978eb5dd18
Adam Pridmore <adam_pridomre@hotmmail.com>
2022-01-24 12:31:54 +0000

    Added Yonk bonk event

--- FsCxData/Scripts/Script_10_YP_2021_RD3.fsx
+++ FsCxData/Scripts/Script_10_YP_2021_RD3.fsx
@@ -20,7 +20,9 @@ let urls = [
     // @"http://results.smartiming.co.uk/view-race/yycard1mv40senior/";     // RD1 (Not on smart timings...)
     // @"http://results.smartiming.co.uk/view-race/yycard2mv40senior/";     // RD2
     // @"http://results.smartiming.co.uk/view-race/yycard3mv40senior/"      // RD3
-    "http://results.smartiming.co.uk/view-race/northernareachampsmjmv40/"   // Northern Area Championships MJ - MV40
+    // "http://results.smartiming.co.uk/view-race/northernareachampsmjmv40/"   // Northern Area Championships MJ - MV40
+    "http://results.smartiming.co.uk/view-race/ycca2022adults/"   // YP - Yonk Bonk
+    
 ]
 
 urls //|> Seq.head |> Seq.singleton

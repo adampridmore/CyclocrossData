ee7c719d8c43e9d16a29bf080042fc4f31d05873
Adam Pridmore <320922+adampridmore@users.noreply.github.com>
2023-11-26 20:53:04 +0000

    Merged old result

--- FsCxData/Scripts/Script_10_YP_2021_RD3.fsx
+++ FsCxData/Scripts/Script_10_YP_2021_RD3.fsx
@@ -20,6 +20,7 @@ let urls = [
     // @"http://results.smartiming.co.uk/view-race/yycard1mv40senior/";     // RD1 (Not on smart timings...)
     // @"http://results.smartiming.co.uk/view-race/yycard2mv40senior/";     // RD2
     // @"http://results.smartiming.co.uk/view-race/yycard3mv40senior/"      // RD3
+    // "http://results.smartiming.co.uk/view-race/yycard7mv40senior/" // - RD7
     // "http://results.smartiming.co.uk/view-race/northernareachampsmjmv40/"   // Northern Area Championships MJ - MV40
     // "http://results.smartiming.co.uk/view-race/ycca2022adults/"   // YP - Yonk Bonk
     // "http://results.smartiming.co.uk/view-race/ycca2022summerr2adults/" // YP Summer RD 2 - York
